0e03ec07fd4b1e18ad6c84645a6e499176f9f9b9
Lucas Pinto <lucas@aslanet.com.br>
2025-09-30 00:28:13 -0300

    shore - menu inicial

--- Projeto branch minha/Colossal-Java-main/Main.java
+++ Projeto branch minha/Colossal-Java-main/Main.java
@@ -3,6 +3,7 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,6 +11,54 @@ public class Main {
         DateTimeFormatter dataformatada = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
         String data = hora.format(dataformatada);
         System.out.println("Hora inicializada: " + data);
+
+        System.out.println("=== MENU ===");
+        System.out.println("1 - Cadastrar Mago");
+        System.out.println("2 - Modos de Jogo");
+        System.out.println("3 - Agendar Partida");
+        System.out.println("0 - Sair");
+        System.out.println("============");
+         Scanner scan = new Scanner(System.in);
+         int escolha = scan.nextInt();
+         scan.close();
+         if (escolha == 1) {
+             System.out.println("Opção 1 selecionada: Cadastrar Mago");
+         } else if (escolha == 2) {
+             System.out.println("Opção 2 selecionada: Modos de Jogo");
+             System.out.println("\n Escolha o modo de jogo:");
+                System.out.println("1 - Modo Treino");
+                System.out.println("2 - Modo 1v1");
+                System.out.println("3 - Modo Equipes (3v3)");
+                System.out.println("4 - Voltar ao Menu Principal");
+                int modoEscolhido = scan.nextInt();
+                switch (modoEscolhido) {
+                    case 1:
+                        System.out.println("Modo Treino selecionado.");
+                        break;
+                    case 2:
+                        System.out.println("Modo 1v1 selecionado.");
+                        break;
+                    case 3:
+                        System.out.println("Modo Equipes (3v3) selecionado.");
+                        break;
+                    case 4:
+                        System.out.println("Voltando ao Menu Principal.");
+                        break;
+                    default:
+                        System.out.println("Opção inválida. Voltando ao Menu Principal.");
+                        break;
+                }
+            } else if (escolha == 3) {
+                System.out.println("Opção 3 selecionada: Agendar Partida");
+            
+         } else if (escolha == 0) {
+             System.out.println("Saindo...");
+
+
+        // System.out.println("\n=== CADASTRO DE UM MAGO ===\n");
+        // System.out.println("Dados do Mago:");
+        
+
         Mago merlin = new Mago("M001", "Merlin", 10000, 100, 100000, 30, 20, "Arcano", "Cajado", 0);
         Mago darkMage = new Mago("M002", "Sombrio", 80, 80, 80, 25, 15, "Sombrio", "Tomo", 0);
         Mago elementalMage = new Mago("M003", "Elementalista", 90, 90, 90, 28, 18, "Elemental", "Vara", 0);
@@ -36,4 +85,5 @@ public class Main {
         merlin.utilizarFeiticoCanalizado(drenagemVida, darkMage, 0);
         System.out.println("Vida do " + darkMage.getCodinome() + ": " + darkMage.getVida());
     }
+  }
 }
